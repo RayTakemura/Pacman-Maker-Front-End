c4ac78bd240e740e29fe4f06d2ce133f8f5b5fc4
Ray Takemura <raytakemura@gmail.com>
2023-10-29 11:33:24 -0700

    create sample canvas

--- src/components/SimpleCanvasExample.tsx
+++ src/components/SimpleCanvasExample.tsx
@@ -1,4 +1,18 @@
+import React, { useRef, useEffect } from "react";
 const SimpleCanvasExample: React.FC = () => {
-  return <canvas></canvas>;
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const canvasCtxRef = React.useRef<CanvasRenderingContext2D | null>(null);
+
+  useEffect(() => {
+    // initiallize
+    if (canvasRef.current) {
+      canvasCtxRef.current = canvasRef.current.getContext("2d");
+      const ctx = canvasCtxRef.current;
+      ctx!.beginPath();
+      ctx!.arc(95, 50, 40, 0, Math.PI * 2);
+      ctx!.stroke();
+    }
+  }, []);
+  return <canvas ref={canvasRef}></canvas>;
 };
 export default SimpleCanvasExample;

e399988b3209129706102afe114ce1849fbf736b
RayTakemura <raytakemura@gmail.com>
2023-11-09 21:02:20 -0800

    debug typescript error messages

--- src/components/PacmanCanvas.tsx
+++ src/components/PacmanCanvas.tsx
@@ -4,15 +4,14 @@ const PacmanCanvas: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const canvasCtxRef = React.useRef<CanvasRenderingContext2D | null>(null);
   function startGame() {
-    console.log("startGame started");
+    console.log("beginning startGame");
     const map = [
       ["-", "-", "-", "-", "-", "-"],
       ["-", " ", " ", " ", " ", "-"],
       ["-", " ", " ", " ", " ", "-"],
       ["-", "-", "-", "-", "-", "-"],
     ];
-    // const boundaries = Array<Boundary>;
-    const boundaries = Array<Boundary>;
+    const boundaries = Array<Boundary>();
     map.forEach((row, i) => {
       row.forEach((symbol, j) => {
         switch (symbol) {
@@ -20,8 +19,8 @@ const PacmanCanvas: React.FC = () => {
             boundaries.push(
               new Boundary({
                 position: {
-                  x: 0 * j,
-                  y: 0 * i,
+                  x: 40 * j,
+                  y: 40 * i,
                 },
                 ctx: canvasCtxRef.current,
               }),
@@ -43,8 +42,9 @@ const PacmanCanvas: React.FC = () => {
       canvasCtxRef.current = can.getContext("2d");
       // const ctx = canvasCtxRef.current;
       startGame();
+      console.log("after startGame()");
     }
-  });
+  }, []);
   return <canvas ref={canvasRef}></canvas>;
 };
 export default PacmanCanvas;
